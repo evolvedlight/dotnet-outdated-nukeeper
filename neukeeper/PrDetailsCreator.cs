04d052b87adddedce40e29f29765e1ec56769d13
Stephen Brown <steve@brown.bg>
2023-11-27 22:51:19 +0100

    Add unit tests, disable releasing

---
+++ neukeeper/PrDetailsCreator.cs
@@ -0,0 +1,81 @@
+﻿using neukeeper.Models;
+using neukeeper.shared;
+using System.Text;
+
+namespace neukeeper;
+
+public class PrDetailsCreator
+{
+    public static PrDetails CreatePrDetails(UpgradeResult upgradeResult)
+    {
+        string? branchName;
+        string? title;
+
+        if (upgradeResult.UpgradedPackages.Count == 1)
+        {
+            var dep = upgradeResult.UpgradedPackages[0];
+            branchName = $"neukeeper/upgrade_{dep.Name}_{dep.LatestVersion}";
+            title = $"Neukeeper: Upgrade {dep.Name} to {dep.LatestVersion}";
+        }
+        else
+        {
+            var count = upgradeResult.UpgradedPackages.Count;
+
+            var hash = CalculateHash(upgradeResult);
+
+            branchName = $"neukeeper/{count}_upgrades_{hash}";
+            title = $"Neukeeper: Upgrade {upgradeResult.UpgradedPackages.Count} packages";
+        }
+
+        var body = new StringBuilder();
+        body.AppendLine("This upgrades the following packages:");
+        body.AppendLine();
+        body.AppendLine("| Project | Package | Old Version | New Version |");
+        body.AppendLine("| - | - | - | - |");
+
+        foreach (var upgrade in upgradeResult.UpgradedPackages)
+        {
+            if (upgrade.Projects != null)
+            {
+                foreach (var project in upgrade.Projects)
+                {
+                    body.AppendLine($"| {project.Project} | {upgrade.Name} | {upgrade.ResolvedVersion} | {upgrade.LatestVersion} |");
+                }
+            }
+            else
+            {
+                body.AppendLine($"|  | {upgrade.Name} | {upgrade.ResolvedVersion} | {upgrade.LatestVersion} |");
+            }
+        }
+
+        if (upgradeResult.UpgradedPackages.All(p => p.IsVersionCentrallyManaged))
+        {
+            body.AppendLine();
+            body.AppendLine("Central package management is used ✔️");
+        }
+
+        return new PrDetails
+        (
+            branchName,
+            title,
+            body.ToString()
+        );
+    }
+
+    private static int CalculateHash(UpgradeResult upgradeResult)
+    {
+        var hash = 0;
+
+        foreach (var package in upgradeResult.UpgradedPackages)
+        {
+            hash = unchecked(hash + package.Name.GetHashCode());
+
+            foreach (var project in package.Projects)
+            {
+                hash = unchecked(hash + project.Project.GetHashCode());
+            }
+        }
+
+        return Math.Abs(hash % 397);
+    }
+}
---
+++ tests/unittests/PrDetailsCreatorTests.cs
@@ -0,0 +1,132 @@
+﻿using DotNetOutdated;
+using neukeeper.shared;
+using static DotNetOutdated.ConsolidatedPackage;
+
+namespace neukeeper.test
+{
+    public class PrDetailsCreatorTests
+    {
+        [Fact]
+        public void CreatePrDetails_SingleUpgrade_CreatesCorrectDetails()
+        {
+            // Arrange
+            var upgradeResult = new UpgradeResult
+            (
+                true,
+                upgradedPackages:
+                [
+                    new ConsolidatedPackage
+                    {
+                        Name = "TestPackage",
+                        LatestVersion = new NuGet.Versioning.NuGetVersion(1, 0, 1),
+                        ResolvedVersion = new NuGet.Versioning.NuGetVersion(1, 0, 0),
+                        Projects = [new PackageProjectReference { Project = "TestProject" }]
+                    }
+                ],
+                upgradedProjects:
+                [
+                    new PackageProjectReference
+                    {
+                        Project = "TestProject",
+                        ProjectFilePath = "TestProject.csproj"
+                    }
+                ]
+            );
+
+            // Act
+            var prDetails = PrDetailsCreator.CreatePrDetails(upgradeResult);
+
+            // Assert
+            Assert.Equal("neukeeper/upgrade_TestPackage_1.0.1", prDetails.BranchName);
+            Assert.Equal("Neukeeper: Upgrade TestPackage to 1.0.1", prDetails.Title);
+            Assert.Contains("| TestProject | TestPackage | 1.0.0 | 1.0.1 |", prDetails.BodyMarkdown);
+        }
+
+        [Fact]
+        public void CreatePrDetails_MultipleUpgrades_CreatesCorrectDetails()
+        {
+            // Arrange
+            var upgradeResult = new UpgradeResult
+            (
+                true,
+                upgradedPackages:
+                [
+                    new ConsolidatedPackage
+                    {
+                        Name = "TestPackage1",
+                        LatestVersion = new NuGet.Versioning.NuGetVersion(1, 0, 1),
+                        ResolvedVersion = new NuGet.Versioning.NuGetVersion(1, 0, 0),
+                        Projects = [new PackageProjectReference { Project = "TestProject1" }]
+                    },
+                    new ConsolidatedPackage
+                    {
+                        Name = "TestPackage2",
+                        LatestVersion = new NuGet.Versioning.NuGetVersion(2, 0, 1),
+                        ResolvedVersion = new NuGet.Versioning.NuGetVersion(2, 0, 0),
+                        Projects = [new PackageProjectReference { Project = "TestProject2" }]
+                    }
+                ],
+                upgradedProjects:
+                [
+                    new PackageProjectReference
+                    {
+                        Project = "TestProject1",
+                        ProjectFilePath = "TestProject1.csproj"
+                    },
+                    new PackageProjectReference
+                    {
+                        Project = "TestProject2",
+                        ProjectFilePath = "TestProject2.csproj"
+                    }
+                ]
+            );
+
+            // Act
+            var prDetails = PrDetailsCreator.CreatePrDetails(upgradeResult);
+
+            // Assert
+            Assert.StartsWith("neukeeper/2_upgrades_", prDetails.BranchName);
+            Assert.Equal("Neukeeper: Upgrade 2 packages", prDetails.Title);
+            Assert.Contains("| TestProject1 | TestPackage1 | 1.0.0 | 1.0.1 |", prDetails.BodyMarkdown);
+            Assert.Contains("| TestProject2 | TestPackage2 | 2.0.0 | 2.0.1 |", prDetails.BodyMarkdown);
+        }
+
+        [Fact]
+        public void CreatePrDetails_CentralPackageManagement_CreatesCorrectDetails()
+        {
+            // Arrange
+            var upgradeResult = new UpgradeResult
+            (
+                true,
+                upgradedPackages:
+                [
+                    new ConsolidatedPackage
+                    {
+                        Name = "TestPackage",
+                        LatestVersion = new NuGet.Versioning.NuGetVersion(1, 0, 1),
+                        ResolvedVersion = new NuGet.Versioning.NuGetVersion(1, 0, 0),
+                        Projects = [new PackageProjectReference { Project = "TestProject" }],
+                        IsVersionCentrallyManaged = true
+                    }
+                ],
+                upgradedProjects:
+                [
+                    new PackageProjectReference
+                    {
+                        Project = "TestProject",
+                        ProjectFilePath = "TestProject.csproj"
+                    }
+                ]
+            );
+
+            // Act
+            var prDetails = PrDetailsCreator.CreatePrDetails(upgradeResult);
+
+            // Assert
+            Assert.Equal("neukeeper/upgrade_TestPackage_1.0.1", prDetails.BranchName);
+            Assert.Equal("Neukeeper: Upgrade TestPackage to 1.0.1", prDetails.Title);
+            Assert.Contains("| TestProject | TestPackage | 1.0.0 | 1.0.1 |", prDetails.BodyMarkdown);
+            Assert.Contains("Central package management is used ✔️", prDetails.BodyMarkdown);
+        }
+    }
+}
